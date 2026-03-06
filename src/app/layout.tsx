546252cc674e3e460b65d3b65ac16ac7ce6b21fc
paktua <arifw.affandi@yahoo.com>
2026-03-06 13:08:49 +0700

    style: mobile container width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 
         <Providers>
 
-          <main className="w-full max-w-md min-h-screen">
+          <main className="w-full max-w-[430px] min-h-screen px-4">
 
             <AppShell>
               {children}
