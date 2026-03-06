7508c0e50850c9ad18e582c8e3dbe469ab4e026b
paktua <arifw.affandi@yahoo.com>
2026-03-06 09:47:04 +0700

    fix dialog component typing for vercel

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -3,7 +3,37 @@
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 
-export const Dialog = DialogPrimitive.Root
+export interface DialogProps extends React.ComponentProps<typeof DialogPrimitive.Root> {}
+
+export function Dialog({ children, ...props }: DialogProps) {
+  return (
+    <DialogPrimitive.Root {...props}>
+      {children}
+    </DialogPrimitive.Root>
+  )
+}
+
 export const DialogTrigger = DialogPrimitive.Trigger
-export const DialogContent = DialogPrimitive.Content
+
+export function DialogContent({
+  children,
+  ...props
+}: React.ComponentProps<typeof DialogPrimitive.Content>) {
+
+  return (
+    <DialogPrimitive.Portal>
+
+      <DialogPrimitive.Overlay className="fixed inset-0 bg-black/40" />
+
+      <DialogPrimitive.Content
+        {...props}
+        className="fixed left-1/2 top-1/2 w-[90%] max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-4 shadow-lg"
+      >
+        {children}
+      </DialogPrimitive.Content>
+
+    </DialogPrimitive.Portal>
+  )
+}
+
 export const DialogClose = DialogPrimitive.Close
