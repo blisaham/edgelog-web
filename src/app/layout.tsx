d8a6ea35386370cb9a3cfdcdffdecc55ca4ff30c
paktua <arifw.affandi@yahoo.com>
2026-03-06 13:34:53 +0700

    improve responsive layout for mobile and desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 
         <Providers>
 
-          <main className="w-full min-h-screen px-4">
+          <main className="w-full min-h-screen px-4 xl:max-w-[640px] xl:mx-auto">
 
             <AppShell>
               {children}
